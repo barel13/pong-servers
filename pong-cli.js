bf52787cc10def755c691e7e6c250d959d580c40
Barel Shlidor <Barel@shlidor.org>
2022-06-27 18:43:00 +0200

    Implement all functionalities of the servers

--- pong-cli.js
+++ pong-cli.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
 
 const args = process.argv.slice(2);
 const server1 = express();
 const server2 = express();
 
 const basePort = 3000;
+const configFile = './config.json';
+
+let shouldPing = true;
+let options = {
+    running: true,
+    destroy: false
+};
+let lastRuntime = true;
 
 if (args.length === 0) {
     console.log("You didn't enter any command, can't run the server. Have a nice day.");
@@ -37,8 +47,34 @@ function parseCommand(arguments) {
     }
 }
 
+/**
+ * Starts the servers after GET request.
+ * @param timeout the timeout between each message in milliseconds.
+ */
 function startServers(timeout) {
+    // write initial configurations.
+    options = {
+        running: true,
+        destroy: false
+    };
+    fs.writeFile(configFile, JSON.stringify(options), (err) => {
+        if (err) {
+            console.log(`There was an error ${err.message}`);
+        }
+    });
+
+
     const serverResponse = (req, __) => {
+        let data = fs.readFileSync(configFile),
+            myObj;
+        myObj = JSON.parse(data);
+
+        if (myObj.destroy) {
+            process.exit(0)
+            return;
+        }
+        if (!myObj.running) return;
+
         const port = req.get("host").split(":")[1];
         pingPong(timeout, port);
     };
@@ -49,18 +85,61 @@ function startServers(timeout) {
     server2.listen(basePort + 1);
 }
 
+/**
+ * print ping/pong and notifies the second server after the specified amount of time.
+ * @param timeout the timeout between each message in milliseconds.
+ * @param port the port of the sending server.
+ */
 function pingPong(timeout, port) {
-console.log("Ping pong")
+    if (new Date().getTime() < lastRuntime + timeout) return;
+
+    console.log(shouldPing ? "Ping" : "Pong");
+    lastRuntime = new Date().getTime();
+    const pingPort = 2 * basePort + 1 - port; // (sum of the ports) => 2 * basePort + 1.
+    // switch between 2 numbers a and b => (a + b) - n where n is either a or b.
+
+    // noinspection JSIgnoredPromiseFromCall
+    setTimeout(() => axios.get(`http://localhost:${pingPort}`), timeout);
+    shouldPing = !shouldPing;
 }
 
+/**
+ * Pause messages between the servers.
+ */
 function pauseServers() {
-
+    options.running = false;
+    fs.writeFile(configFile, JSON.stringify(options), (err) => {
+        if (err) {
+            console.log("couldn't save the data");
+            console.log(err.message);
+        }
+    });
 }
 
+/**
+ * Resume the messages between the servers.
+ */
 function resumeServers() {
-
+    options.running = true;
+    fs.writeFile(configFile, JSON.stringify(options), (err) => {
+        if (err) {
+            console.log("couldn't save the data");
+            console.log(err.message);
+            return;
+        }
+        axios.get(`http://localhost:${basePort}`);
+    });
 }
 
+/**
+ * Stop the servers.
+ */
 function stopServers() {
-
+    options.destroy = true;
+    fs.writeFile(configFile, JSON.stringify(options), (err) => {
+        if (err) {
+            console.log("couldn't save the data");
+            console.log(err.message);
+        }
+    });
 }
